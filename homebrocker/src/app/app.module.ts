b3501347de74b66a9f3ae0e192013804d121d0df
BrunoTravassos <brunodefrontin@gmail.com>
2021-06-08 10:21:48 -0300

    Projeto BootCamp Santander

---
+++ homebrocker/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { from } from 'rxjs';
+
+import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { AppRoutingModule } from './app-routing.module'
+import { SharedModule } from './shared/shared.module';
+import { registerLocaleData } from '@angular/common';
+
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    DashboardModule,
+    AppRoutingModule,
+    SharedModule,
+  ],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt-BR'}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
